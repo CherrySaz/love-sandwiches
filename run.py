d8cf95f632de647d63b016d619a071cdd6ae53e6
CherrySaz <>
2023-09-09 10:21:40 +0000

    collect sales data for last 5 months sales in market in lists of columns

--- run.py
+++ run.py
@@ -62,6 +62,7 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+
 def update_surplus_worksheet(data):
     """
     Update surplus worksheet, add new row with the list data provided
@@ -71,7 +72,7 @@ def update_surplus_worksheet(data):
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
 
- 
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -82,12 +83,27 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
     return surplus_data
+
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
@@ -103,4 +119,4 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
